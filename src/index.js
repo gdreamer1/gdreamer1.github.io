eef168c48f843e3b149e016ab50dc3cb2cfaf05a
gdreamer1 <gdadreamer@gmail.com>
2021-03-26 18:17:18 -0700

    change list position

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 alert ("Welcome to my site! Please feel free to explore and get to know more about me")
+
+const begin = document.getElementsByClassName('card begin')[0]
+const beginTitle = begin.getElementsByTagName(h1)[0]
+
+beginTitle.innerText = "Giovanni Harper"
